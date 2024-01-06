a6279260e3422a72de35b79ee55c9eb99ee78f71
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-01-06 14:23:09 +0500

    typescript

--- array.ts
+++ array.ts
@@ -1,18 +1,24 @@
 // var array = ["ahmad","ali","asim","zain","talal"]
 // console.log(array);
 
-function names() {
-   var names:Array<string | number |boolean>= ["ahmad",true,7,"ali","asim","zain","talal"]
-   console.log("names is : ",names);
+// function names() {
+//    var names:Array<string | number |boolean>= ["ahmad",true,7,"ali","asim","zain","talal"]
+//    console.log("names is : ",names);
+// }
+// names()
+// function names1() {
+//     var names1:Array<string | number |boolean>= ["abdur rahman",7,"ali","asim","zain","talal"]
+//     names1.push("awais");
+//     names1.shift();
+//     names1.unshift("kashif")
+//     names1.pop()
+//     names1.splice(1 , 4 ,"ahmad")
+//     console.log("names is : ",names1);
+//  }
+//  names1()
+
+for (let index = 0; index <10; index++) {
+   const element =[index];
+   console.log("Ahmad");
+   
 }
-names()
-function names1() {
-    var names1:Array<string | number |boolean>= ["abdur rahman",7,"ali","asim","zain","talal"]
-    names1.push("awais");
-    names1.shift();
-    names1.unshift("kashif")
-    names1.pop()
-    names1.splice(1 , 4 ,"ahmad")
-    console.log("names is : ",names1);
- }
- names1()
