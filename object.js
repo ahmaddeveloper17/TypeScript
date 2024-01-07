770ba19aa5cb9b649475730ad00c7c4dd2acabed
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-01-07 18:48:17 +0500

    typescript

--- object.js
+++ object.js
@@ -1,12 +1,18 @@
-var names = {
-    name: "Ahmad",
-    name2: "ali",
-    name3: "awais",
-    name4: "talal",
-    name5: "Ahmad",
-    name6: "Ahmad",
-    name7: "Ahmad",
-    name8: "Ahmad",
-    name9: "Ahmad",
-};
-console.log(names);
+// let names = {
+//     name: "Ahmad",
+//     name2: "ali",
+//     name3: "awais",
+//     name4: "talal",
+//     name5: "Ahmad",
+//     name6: "Ahmad",
+//     name7: "Ahmad",
+//     name8: "Ahmad",
+//     name9: "Ahmad",
+// }
+// console.log(names);
+console.log("gg");
+function hello() {
+    console.log("Ahmad is a good boy");
+}
+setTimeout(hello, 5000);
+console.log("hh");
--- object.ts
+++ object.ts
@@ -1,12 +1,24 @@
-let names = {
-    name: "Ahmad",
-    name2: "ali",
-    name3: "awais",
-    name4: "talal",
-    name5: "Ahmad",
-    name6: "Ahmad",
-    name7: "Ahmad",
-    name8: "Ahmad",
-    name9: "Ahmad",
+// let names = {
+//     name: "Ahmad",
+//     name2: "ali",
+//     name3: "awais",
+//     name4: "talal",
+//     name5: "Ahmad",
+//     name6: "Ahmad",
+//     name7: "Ahmad",
+//     name8: "Ahmad",
+//     name9: "Ahmad",
+// }
+// console.log(names);
+
+
+
+
+
+console.log("gg");
+
+function hello() {
+    console.log("Ahmad is a good boy");
 }
-console.log(names);
+setTimeout (hello,5000)
+console.log("hh");
